667c9e12f46210d19b592d473a0114f28bb6f66b
Michael Porter <michael@muddybootscode.io>
2021-08-06 13:23:36 -0500

    amplication-build-cks0of7fd1496691bs64lvhry5a

--- admin-ui/src/api/project/ProjectWhereInput.ts
+++ admin-ui/src/api/project/ProjectWhereInput.ts
@@ -1,14 +1,17 @@
+import { DateTimeFilter } from "../../util/DateTimeFilter";
 import { StringNullableFilter } from "../../util/StringNullableFilter";
 import { DateTimeNullableFilter } from "../../util/DateTimeNullableFilter";
 import { StringFilter } from "../../util/StringFilter";
 import { UserWhereUniqueInput } from "../user/UserWhereUniqueInput";
 
 export type ProjectWhereInput = {
+  createdAt?: DateTimeFilter;
   description?: StringNullableFilter;
   dueDate?: DateTimeNullableFilter;
   id?: StringFilter;
   name?: StringFilter;
   owner?: StringFilter;
   startDate?: DateTimeNullableFilter;
+  updatedAt?: DateTimeFilter;
   user?: UserWhereUniqueInput;
 };
